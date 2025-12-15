1bd79ee71b40a21bd6c0a6aa80a6b6d13470541b
Om Raheja <om@omraheja.me>
2025-12-15 11:02:38 -0500

    made it good

--- migrations/20251215144032_notnull.sql
+++ migrations/20251215144032_notnull.sql
@@ -1,3 +1,11 @@
+UPDATE students_classes 
+SET description = '' 
+WHERE description IS NULL;
+
+UPDATE students_classes 
+SET methods = ARRAY[''] 
+WHERE methods IS NULL;
+
 ALTER TABLE students_classes
 ALTER COLUMN methods SET NOT NULL;
 
