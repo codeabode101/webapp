957ed0bf1fa33f19dbab4d39db4ad5729cdb3b5a
Om Raheja <om@omraheja.me>
2026-02-02 05:12:42 -0500

    fixed the migration

--- migrations/20260131190547_email.sql
+++ migrations/20260131190547_email.sql
@@ -1,9 +1,8 @@
--- Notes and the following will be AI generated
 -- Homework_notes too when that part is updated
-ALTER TABLE students_classes ADD COLUMN taught_methods TEXT[];
-ALTER TABLE students_classes ADD COLUMN needs_practice TEXT[];
+ALTER TABLE students_classes ADD COLUMN IF NOT EXISTS taught_methods TEXT[];
+ALTER TABLE students_classes ADD COLUMN IF NOT EXISTS needs_practice TEXT[];
 
-ALTER TABLE accounts ADD COLUMN email TEXT;
+ALTER TABLE accounts ADD COLUMN IF NOT EXISTS email TEXT;
 
 -- When there is a new class, vercel script will check this to see if the email should be sent on Thursday
-ALTER TABLE students ADD COLUMN sent_email BOOLEAN NOT NULL DEFAULT FALSE;
+ALTER TABLE students ADD COLUMN IF NOT EXISTS sent_email BOOLEAN NOT NULL DEFAULT FALSE;
